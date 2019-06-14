db29d270802b82eb69aaf45102b19dc045c50907
suziemiri <51316354+suziemiri@users.noreply.github.com>
2019-06-14 19:21:15 +0200

    Update scripts.js
    Added var repository

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,33 @@
-alert('Hello World');
+var repository = [
+{
+  name: 'Charmander'
+  height: 0.8 //centimeters
+  types: ['Fire']
+  weaknesses: ['Ground', 'Rock', 'Water']
+},
+
+{
+  name: 'Squirtle'
+  height: 0.4 //centimeters
+  types: ['Water']
+  weaknesses: ['Electric', 'Grass']
+},
+
+{
+  name: 'Pikachu'
+  height: 0.4 //centimeters
+  types: ['Electric']
+  weaknesses: ['Ground']
+},
+
+{
+  name: 'Pidgey'
+  height: 0.4 //centimeters
+  types: ['Normal', 'Flying']
+  weaknesses: ['Electric', 'Ice','Rock']
+},
+];
+
+for (var i = 0; i <= 3; i++) {
+  console.log(i);
+}
