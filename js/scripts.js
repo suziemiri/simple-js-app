f5124ab776569536c9329e1be99c22c750935415
suziemiri <51316354+suziemiri@users.noreply.github.com>
2019-06-28 15:55:29 +0200

    Update scripts.js
    Corrected mistakes

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,33 @@
 var repository = [
 {
-  name: 'Charmander'
-  height: 0.8 //centimeters
-  types: ['Fire']
-  weaknesses: ['Ground', 'Rock', 'Water']
+  name: 'Charmander',
+  height: 0.8, //centimeters
+  types: ['Fire'],
+  weaknesses: ['Ground', 'Rock', 'Water'],
 },
 
 {
-  name: 'Squirtle'
-  height: 0.4 //centimeters
-  types: ['Water']
-  weaknesses: ['Electric', 'Grass']
+  name: 'Squirtle',
+  height: 0.4, //centimeters
+  types: ['Water'],
+  weaknesses: ['Electric', 'Grass'],
 },
 
 {
-  name: 'Pikachu'
-  height: 0.4 //centimeters
-  types: ['Electric']
-  weaknesses: ['Ground']
+  name: 'Pikachu',
+  height: 0.4, //centimeters
+  types: ['Electric'],
+  weaknesses: ['Ground'],
 },
 
 {
-  name: 'Pidgey'
-  height: 0.4 //centimeters
-  types: ['Normal', 'Flying']
-  weaknesses: ['Electric', 'Ice','Rock']
+  name: 'Pidgey',
+  height: 0.4, //centimeters
+  types: ['Normal', 'Flying'],
+  weaknesses: ['Electric', 'Ice','Rock'],
 },
 ];
 
 for (var i = 0; i <= 3; i++) {
-  console.log(i);
+  console.log(repository[i]);
 }
