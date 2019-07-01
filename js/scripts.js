429466c4e39338d13986dc05873f92f9f584a628
suziemiri <51316354+suziemiri@users.noreply.github.com>
2019-07-01 11:00:53 +0200

    Update scripts.js
    added more functions

--- js/scripts.js
+++ js/scripts.js
@@ -29,5 +29,15 @@ var repository = [
 ];
 
 for (var i = 0; i < repository.length; i++) {
-  document.write(repository[i]);
+  if (repository[i].height == 0.8) {
+    document.write(repository[i].name + ' (height: ' + repository[i].height') ');
+  } else if (repository[i].types[0] === 'Water') {
+    document.write(repository[i].name + ' (height: ' + repository[i].height') Wow thats big! ') ');
+  } else if (repository[i].types[0] === 'Fire') { 
+     document.write(repository[i].name + ' (height: ' + repository[i].height') ');
+   } else if (repository[i].types[1] === 'Electric') {  
+    document.write(repository[i].name + ' (height: ' + repository[i].height') ');
+   } else {
+     document.write(repository[i].name + '(height: ' + repository[i].height') ');
+   }
 }
